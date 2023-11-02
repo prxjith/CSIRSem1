cf4a5b4e2db8572fc01d607fe7bd54406716b181
prxjith <143489630+prxjith@users.noreply.github.com>
2023-11-02 21:30:45 +0530

    Update w1p4.cpp

--- Week1/w1p4.cpp
+++ Week1/w1p4.cpp
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,c,d,e,s;
+	int a,b,c,d,e,p;
 	printf("Enter the marks of 5 subjects out of 100: ");
 	scanf("%d",&a);
 	scanf("%d",&b);
 	scanf("%d",&c);
 	scanf("%d",&d);
 	scanf("%d",&e);
-	b=(a+b+c+d+e)/5;
-	printf("The percentage is: %d",b);
+	p=(a+b+c+d+e)/5;
+	printf("The percentage is: %d",p);
 	return 0;
 }
