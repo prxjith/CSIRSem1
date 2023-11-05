41e20522888cd267d497c970ca9d35b16cac1a62
prxjith <143489630+prxjith@users.noreply.github.com>
2023-11-06 01:32:18 +0530

    Update p21.cpp

--- Week5/p21.cpp
+++ Week5/p21.cpp
@@ -2,11 +2,16 @@
 int main()
 {
 	int a[3][3],i,j;
+	printf("Enter the values of the matrix: ");
 	for(i=0;i<3;i++)
-	for(j=0;j<3;j++)
+	for(j=0;j<3;j++) 
 	scanf("%d",&a[i][j]);
+	printf("The matrix is: \n");
     for(i=0;i<3;i++)
+    {
 	for(j=0;j<3;j++)
-	printf("%d",a[i][j]);
+	printf("%d ",a[i][j]);
+	printf("\n");
+    }
 	return 0;
 }
