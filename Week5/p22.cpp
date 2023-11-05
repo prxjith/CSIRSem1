8b3b1d9aa99da6d875bf9a079fd817709aa1f447
prxjith <143489630+prxjith@users.noreply.github.com>
2023-11-06 02:12:54 +0530

    Update p22.cpp

--- Week5/p22.cpp
+++ Week5/p22.cpp
@@ -2,7 +2,7 @@
 int main()
 {
 	int a[3][3],b[3][3],c[3][3],d[3][3],i,j;
-	printf("Emter the values for 1st matrix: \n");
+	printf("Enter the values for 1st matrix: \n");
 	for(i=0;i<3;i++)
 	for(j=0;j<3;j++)
 	scanf("%d",&a[i][j]);
@@ -12,17 +12,23 @@ int main()
 	scanf("%d",&b[i][j]);
 	printf("The result after addition is: \n");
 	for(i=0;i<3;i++)
+	{
 	for(j=0;j<3;j++)
 	{
 	c[i][j]=a[i][j]+b[i][j];
-	printf("%d",c[i][j]);
+	printf("%d ",c[i][j]);
+    }
+    printf("\n");
     }
     printf("The result after subtraction is: \n");
 	for(i=0;i<3;i++)
+	{
 	for(j=0;j<3;j++)
 	{
 	d[i][j]=a[i][j]-b[i][j];
-	printf("%d",d[i][j]);
+	printf("%d ",d[i][j]);
+    }
+    printf("\n");
     }
 	return 0;
 }
